22dd67653f04383f594ec760ab39d263430fbd1b
Crowdbotics <team@crowdbotics.com>
2024-04-03 13:39:10 +0000

    Initial commit

---
+++ backend/icy_water_47374/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for icy_water_47374 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "icy_water_47374.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "icy-water-47374",
+  url: "https://icy-water-47374.botics.co",
+  api: "https://icy-water-47374.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
